6df333011bc6b5c2c31d51011561140e32e9a9c5
SantonioTheFirst <santoniothefirst123@gmail.com>
2022-01-15 19:38:04 +0300

    Code refactoring

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.datastructures import Address
+import requests
+import json
 
-from tensorflow.keras.models import load_model
+# from tensorflow.keras.models import load_model
 
-# import numpy as np
-from numpy import expand_dims, asarray, round
-from numpy.random import normal
+import numpy as np
 
 from matplotlib import pyplot as plt
 
@@ -17,11 +17,14 @@ import io
 
 # np.random.seed(1000)
 # gan = load_model('models/result_gan_nontrainable.h5')
-generator = load_model('models/generator.h5')
+# generator = load_model('models/generator.h5')
 latent_dims = 100
 number_of_photos = 25
 
-cats_dogs_classifier = load_model('models/cats_vs_dogs.h5')
+cats_vs_dogs_URL = 'https://kovalevskiy-cats-vs-dogs.herokuapp.com/'
+ganime_URL = 'https://kovalevskiy-ganime.herokuapp.com/'
+
+# cats_dogs_classifier = load_model('models/cats_vs_dogs.h5')
 
 
 app = FastAPI(docs_url=None, redoc_url=None)
@@ -43,8 +46,8 @@ def plot_images(images):
 
 
 def prediction_result(prediction):
-    return f'Cat probability: {round(prediction[0][0] * 100, 4)}%, \
-         Dog probability: {round(prediction[0][1] * 100, 4)}%'
+    return f'Cat probability: {round(prediction[0] * 100, 4)}%, \
+         Dog probability: {round(prediction[1] * 100, 4)}%'
 
 
 @app.get('/', response_class=HTMLResponse)
@@ -66,14 +69,12 @@ async def cats_vs_dogs(request: Request, file_input: UploadFile = File(...)):
     if 'image' in file_format:
         file_data = await file_input.read()
         im = Image.open(io.BytesIO(file_data))
-        im_arr = asarray(asarray(im))
+        im_arr = np.asarray(im)
         filename = 'image.jpg'
         im.save(f'static/{filename}')
         im.close()
-        prediction = prediction_result(
-            cats_dogs_classifier.predict(
-                expand_dims(
-                    im_arr, axis=0)))
+        prediction = requests.post(cats_vs_dogs_URL, json=json.dumps(im_arr.tolist())).json()['prediction']
+        prediction = prediction_result(prediction)
         image = True
     else:
         prediction = 'Select an image file!'
@@ -93,8 +94,13 @@ def ganime(request: Request):
     # prediction = gan.layers[0](np.random.normal(
     #     0, 1, (number_of_photos, latent_dims)))
 
-    prediction = generator.predict(normal(0, 1, (number_of_photos, latent_dims)))
+    # prediction = generator.predict(normal(0, 1, (number_of_photos, latent_dims)))
 
-    plot_images(prediction)
+    images = []
+    for i in range(number_of_photos):
+        image = requests.get(ganime_URL).json()['Image'][0]
+        images.append(image)
+    images = np.array(images)
+    plot_images(images)
     return templates.TemplateResponse(
         'ganime.html', {'request': request, 'img': True})
